0ad988fdd7cc7ad15c57c55100194b28d713aa24
Iván Peralta <iperalta@mrmilu.com>
2020-08-20 10:51:51 +0200

    Create mrmilu_render_entity for nodes, terms, custom entities...

---
+++ src/Twig/MrMiluRenderEntity.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\mrmilu_twig\Twig;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * Class DefaultService.
+ *
+ * @package Drupal\mrmilu_twig
+ */
+class MrMiluRenderEntity extends \Twig_Extension {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs an EntityViewBuilder object.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   * This function must return the name of the extension. It must be unique.
+   */
+  public function getName() {
+    return 'mrmilu_render_entity';
+  }
+
+  /**
+   * In this function we can declare the extension function
+   */
+  public function getFunctions() {
+    return array(
+      new \Twig_SimpleFunction('mrmilu_render_entity', [$this, 'mrmilu_render_entity']),
+    );
+  }
+
+  /**
+   * The php function to render a given entity
+   */
+  public function mrmilu_render_entity($entityId, $entityType, $displayMode) {
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $entity = \Drupal::entityTypeManager()->getStorage($entityType)->load($entityId);
+
+    $build = $this->entityTypeManager
+      ->getViewBuilder($entityType)
+      ->view($entity, $displayMode, $langcode);
+
+    CacheableMetadata::createFromRenderArray($build)
+      ->merge(CacheableMetadata::createFromObject($entity))
+      ->applyTo($build);
+    return $build;
+  }
+}
